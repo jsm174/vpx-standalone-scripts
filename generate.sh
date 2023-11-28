38ed1dee5641f47591ad42edd6e5b10fa3485542
Jason Millard <jsm174@gmail.com>
2023-11-28 08:00:35 -0500

    misc: rename dmd hack files. update generate.sh to make dmd patch files

--- generate.sh
+++ generate.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-find_files() {
-    local dir="$1"
-    local pattern="$2"
-    
-    for file in "$dir"/*; do
+find_table_patches() {
+    for file in "$1"/*; do
         if [[ -d "$file" ]]; then
-            find_files "$file" "$pattern"
-        elif [[ -f "$file" && "$file" == *"$pattern" ]]; then
-
+            find_table_patches "$file" ".vbs"
+        elif [[ -f "$file" && "$file" == *".vbs" ]]; then
 	    if [[ -f "$file.original" ]]; then
 	       diff -w -u "$file.original" "$file" > "$file.patch"
             else
@@ -18,4 +14,19 @@ find_files() {
     done
 }
 
-find_files "." ".vbs"
+find_dmd_patches() {
+    for file in "$1"/*; do
+        if [[ -d "$file" ]]; then
+            find_dmd_patches "$file" ".vbs"
+        elif [[ -f "$file" && "$file" == *".vbs" ]]; then
+            if [[ -f "$file.dmd" ]]; then
+               diff -w -u "$file" "$file.dmd" > "$file.dmd.patch"
+            else
+               echo "Missing $file.dmd"
+            fi
+        fi
+    done
+}
+
+find_table_patches .
+find_dmd_patches .
